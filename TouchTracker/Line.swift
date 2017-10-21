3de2eb0a10dd37ecb7e02b8e938f7d400d41c22b
Daniel Espina <dannyj.espina@gmail.com>
2017-10-20 22:53:47 -0400

    finished chapter 18 w/ challenges

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,42 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Danny Espina on 10/20/17.
+//  Copyright © 2017 Danny Espina. All rights reserved.
+//
+
+import UIKit
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
+extension Line {
+    
+    var angleDegree : CGFloat {
+        
+        guard begin != end else { return 0 }
+        
+        let dX = end.x - begin.x
+        let dY = end.y - begin.y
+        
+        var angle = atan2(dY, dX) * 180 / CGFloat(Double.pi)
+        
+        //make negative angles be positive and angles can go from 0 to 360
+        if angle < 0 {
+            angle = angle + 360
+        }
+        
+        return CGFloat(angle)
+        
+    }
+    
+    var color: UIColor {
+        let hueCode = angleDegree / 360
+        
+        return UIColor(hue: hueCode, saturation: 1, brightness: 1, alpha: 0.6)
+    }
+    
+}
